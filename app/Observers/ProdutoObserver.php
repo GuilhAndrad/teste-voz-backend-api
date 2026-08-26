ec216684a30fd58f64dc15deea08cc2d123a17d3
GuilhAndrad <wilhelmandrad@gmail.com>
2026-08-26 12:28:41 -0300

    first commit

---
+++ app/Observers/ProdutoObserver.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Produto;
+
+class ProdutoObserver
+{
+    /**
+     * Handle the Produto "created" event.
+     */
+    public function created(Produto $produto): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Produto "updated" event.
+     */
+    public function updated(Produto $produto): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Produto "deleted" event.
+     */
+    public function deleted(Produto $produto): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Produto "restored" event.
+     */
+    public function restored(Produto $produto): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Produto "force deleted" event.
+     */
+    public function forceDeleted(Produto $produto): void
+    {
+        //
+    }
+}
